9d18ae15e094c1ae56723e6fc10ea5a5abca7687
Kumud Gupta <kumud@hudle.in>
2020-07-29 12:51:17 +0530

    intial commit

---
+++ app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AssignmentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    function everything_in_tags($string, $tagname)
+    {
+        $pattern = "#<\s*?$tagname\b[^>]*>(.*?)</$tagname\b[^>]*>#s";
+        preg_match_all($pattern, $string, $matches);
+        return $matches[1];
+    }
+
+    public function assignment(){
+
+        logger("inside controller");
+        $client = new \GuzzleHttp\Client();
+        
+        $response = $client->request('GET', 'https://time.com');
+
+        $matches = $this->everything_in_tags($response->getBody(), 'h2');
+
+        $res = [];
+        $i=0;
+        while($i<5){
+            $result = explode("/", $matches[$i]);
+            if(!empty($result)) {
+                array_push($res, [ 'title' => strip_tags($matches[$i]), 'link' => "https://time.com/$matches[1]/$matches[2]/" ]);
+                $i++;
+            }
+        }
+        logger("Leaving controller");
+
+        return response()->json($res);
+
+    }
+}
